945757d613e6a05f0897d366366172b79398e6dc
Brendan Forster <brendan@github.com>
2015-04-27 15:23:03 +0930

    wireup bindings for footers

---
+++ FluxSharp.UI/Components/FooterView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using FluxSharp.Abstractions;
+using FluxSharp.Actions;
+using FluxSharp.Stores;
+
+namespace FluxSharp.Components
+{
+    public partial class FooterView : IFluxViewFor<ToDoStore>
+    {
+        public FooterView()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+            {
+                return;
+            }
+
+            this.OnChange(store =>
+            {
+                var items = store.GetAll();
+                var completed = items.Count(x => x.IsComplete);
+                var incomplete = items.Count() - completed;
+
+                counter.Text = string.Format("{0} item{1} left",
+                    incomplete, incomplete > 1 ? "s" : "");
+
+                clear.Visibility = completed > 0 ? Visibility.Visible : Visibility.Collapsed;
+
+                if (completed > 0)
+                {
+                    clear.Events().Click.Subscribe(_ =>
+                    {
+                        this.Dispatch(new ClearCompletedTasksAction());
+                    });
+                }
+            });
+        }
+    }
+}
