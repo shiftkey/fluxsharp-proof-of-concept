94b254fa67a427aaf81edd01a18db4bf20008eb6
Steven Atkinson <steven.atkinson@gmx.com>
2016-04-09 19:53:49 +0100

    Refactored Dispatcher methods utilizing some of Linqs awesomeness.

--- FluxSharp.UI/Abstractions/Dispatcher.cs
+++ FluxSharp.UI/Abstractions/Dispatcher.cs
@@ -1,53 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FluxSharp.Abstractions
 {
     public class Dispatcher
     {
-        readonly Dictionary<Type, List<object>> callbacks
-            = new Dictionary<Type, List<object>>();
+        private readonly IDictionary<Type, IList<object>> _callbacks =
+            new Dictionary<Type, IList<object>>();
 
         public void Register<T>(Action<T> callback)
         {
             var key = typeof(T);
 
-            if (callbacks.ContainsKey(key))
-            {
-                var currentCallbacks = callbacks[key];
-                currentCallbacks.Add(callback);
-            }
-            else
-            {
-                var newCallbacks = new List<object> { callback };
-                callbacks.Add(key, newCallbacks);
-            }
+            if(!_callbacks.ContainsKey(key))
+                _callbacks.Add(key, new List<object>());
+
+            _callbacks[key].Add(callback);
         }
 
         public void Dispatch<T>(T payload)
         {
             var key = typeof(T);
 
-            if (callbacks.ContainsKey(key))
-            {
-                var currentCallbacks = callbacks[key];
-                foreach (var c in currentCallbacks)
-                {
-                    Action<T> convertedCallback = c as Action<T>;
-                    if (convertedCallback == null)
-                    {
-                        // TODO: whut?
-                    }
-                    else
-                    {
-                        convertedCallback(payload);
-                    }
-                }
-            }
-            else
-            {
-                // TODO: whut?
-            }
+            if (!_callbacks.ContainsKey(key))
+                return;
+
+            _callbacks[key]
+                .OfType<Action<T>>()
+                .ToList()
+                .ForEach(callback => callback(payload));
         }
     }
 }
